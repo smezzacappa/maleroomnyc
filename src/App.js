55ac273486e42afaa0fc35e5088411d0a4030eca
Steven Mezzacappa <steven.mezzacappa@gmail.com>
2019-08-29 19:52:15 -0400

    Coming Soon

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+import Logo from './components/Logo/Logo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+        <div className="h1">Coming Soon</div>  
+        <p>
+          <Logo/>
+        </p>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import maleroom from './maleroom.png';
+
+const Logo = () =>{
+   
+    return(
+
+<img src={maleroom} alt="maleroom"></img>
+
+    )
+};
+
+export default Logo;
