0e300fe6f17c1d4aa39d182c976f0eaaa11afe1e
Steven Mezzacappa <steven.mezzacappa@gmail.com>
2019-08-29 20:51:57 -0400

    Updates

---
+++ precache-manifest.51aca72a3facb0a703321b5a1ef13b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f072abe7c0f2ca4ff7b0250236e600",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b95e57bcdbba46641f39",
+    "url": "/static/css/main.4b8b0c56.chunk.css"
+  },
+  {
+    "revision": "891fca3693e629099c61",
+    "url": "/static/js/2.52dffd02.chunk.js"
+  },
+  {
+    "revision": "b95e57bcdbba46641f39",
+    "url": "/static/js/main.77fdb223.chunk.js"
+  },
+  {
+    "revision": "0898480d81576319ba6b",
+    "url": "/static/js/runtime~main.38607086.js"
+  },
+  {
+    "revision": "630b999d05d783494087baebc3dd62b8",
+    "url": "/static/media/maleroom.630b999d.png"
+  }
+]);
